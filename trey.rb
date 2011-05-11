3b0ab22261418aa5bd2649bd6e403b57d5093df3
Mike Skwarcan <mikeskwarcan@Mike-Skwarcans-iMac.local>
2011-05-11 18:27:29 -0500

    Home page and features

--- trey.rb
+++ trey.rb
@@ -5,3 +5,23 @@ Bundler.setup
 get '/' do
   erb :home
 end
+
+get '/features' do
+  erb :features
+end
+
+get '/applications' do
+  erb :applications
+end
+
+get '/specifications' do
+  erb :specifications
+end
+
+get '/resellers' do
+  erb :resellers
+end
+
+get '/contact' do
+  erb :contact
+end
